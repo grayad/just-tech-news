cb025b3bad04fb0d69edc77106dd0c89036c9bc5
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-07-24 09:33:29 -0400

    create Post model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 // import User model
 const User = require('./User');
+const Post = require("./Post");
 
 // export an object with User model as a property
-module.exports = { User };
+module.exports = { User, Post };
