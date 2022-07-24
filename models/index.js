fe8142a6c5cfd81b9d463bf683fe8adf445c44d3
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-07-24 19:38:21 -0400

    add vote model and routes

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // import User model
 const User = require('./User');
 const Post = require("./Post");
+const Vote = require('./Vote');
 
 // create associations
 // a user can have multiple posts
@@ -11,6 +12,28 @@ User.hasMany(Post, {
 Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
 
 // export an object with User model as a property
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
